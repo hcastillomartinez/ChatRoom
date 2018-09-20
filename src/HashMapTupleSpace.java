998aaf5e4b83f463094450cf7b01223f7df3b8ef
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2018-09-20 00:54:08 -0600

    hashmap implementation done

--- src/HashMapTupleSpace.java
+++ src/HashMapTupleSpace.java
@@ -1,20 +1,69 @@
+package sample;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class HashMapTupleSpace extends TupleSpace{
+//    private List<Tuple> listN=new ArrayList<>();
+    HashMap<Integer,List<Tuple>>map=new HashMap<>();
 
-    HashMap<Tuple,Integer>map=new HashMap<>();
-
-
-    public void add(Tuple tuple){
-        map.put(tuple,tuple.getSize());
+    /**
+     * Adds tuple to Tuple space first checking if List of the tuples size already
+     * exists. If it doesn't then key is size of Tuple(number of objects in pattern) and value
+     * is a List is then added to HashMap. If it already exists then simply added to
+     * List.
+     * @param tuple The Tuple to be added to the tuple space
+     */
+    public void add(Tuple tuple) {
+        //list of tuple size doesnt exist yet
+        if (map.containsKey(tuple.getSize()) == false) {
+            List<Tuple> list = new ArrayList<>();
+            map.put(tuple.getSize(), list);
+            map.get(tuple.getSize()).add(tuple);
+        }
+        else {
+            //if tuple doesn't exist in list then add it
+            if (map.get(tuple.getSize()).contains(tuple) == false) {
+                map.get(tuple.getSize()).add(tuple);
+            }
+        }
     }
+
+    /**
+     * Removes a Tuple that matches the pattern passed in as argument from TupleSpace.
+     * @param pattern Tuple's objects
+     * @return Returns Tuple that was removed from TupleSpace, if not found returns the one searched for
+     */
     public Tuple remove(Object... pattern){
-        Tuple p=new Tuple();
+        Tuple p=new Tuple(pattern);
+        if(map.containsKey(p.getSize())==true){
+            List<Tuple> list=map.get(p.getSize());
+            for(Iterator<Tuple> iterator=list.iterator();iterator.hasNext();){
+                Tuple temp=iterator.next();
+                if(temp.isEqual(p)==true){
+                    iterator.remove();
+                    return temp;
+                }
+            }
+        }
         return p;
     }
+
+    /**
+     *Checks if pattern exists in a Tuple in the TupleSpace
+     * @param pattern Tuple's objects
+     * @return Returns A Tuple that matches pattern with pattern in parameter.
+     */
     public  Tuple read(Object... pattern){
-        Tuple p=new Tuple();
+        Tuple p=new Tuple(pattern);
+        if(map.containsKey(p.getSize())==true){
+            List<Tuple> list=map.get(p.getSize());
+            for(Iterator<Tuple> iterator=list.iterator();iterator.hasNext();){
+                Tuple temp=iterator.next();
+                if(temp.isEqual(p)==true){
+                    return temp;
+                }
+            }
+        }
         return p;
     }
 }

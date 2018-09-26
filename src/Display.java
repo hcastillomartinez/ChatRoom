a63e4df30fea185d796256abb5c784e98e63083b
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2018-09-25 23:24:45 -0600

    display change and commit of linkedList implementation

--- src/Display.java
+++ src/Display.java
@@ -1,15 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -18,72 +13,80 @@ public class Display extends Application {
     @Override
     public void start(Stage primaryStage){
         AnchorPane root=new AnchorPane();
-        root.setPrefSize(380,467);
+        root.setPrefSize(380,500);
         root.setStyle("-fx-background-color: #3333ff");
 
-        Label status= new Label();
-        status.setLayoutX(30);
-        status.setLayoutY(63);
-        status.setPrefSize(81,42);
-        status.setStyle("-fx-background-color: lightBlue; -fx-background-radius: 10;"+"-fx-font-size: 16");
-        status.setText("Active");
-        status.setPadding(new Insets(0,0,0,15));
+        MenuBar menuBar=new MenuBar();
+        menuBar.setPrefSize(380,25);
+        menuBar.setLayoutX(0);
+        menuBar.setLayoutY(0);
+        Menu menu=new Menu("Menu");
+        MenuItem addUser=new MenuItem("Add User");
+        menu.getItems().add(addUser);
+        menuBar.getMenus().add(menu);
+        root.getChildren().add(menuBar);
 
-        Label userView=new Label();
-        userView.setLayoutX(278);
-        userView.setLayoutY(63);
-        userView.setPrefSize(81,42);
-        userView.setStyle("-fx-background-color: lightBlue; -fx-background-radius: 10;"+"-fx-font-size: 16");
-        userView.setText("All");
-        userView.setPadding(new Insets(0,0,0,30));
-        root.getChildren().add(userView);
-        root.getChildren().add(status);
+        Button activeButton=new Button("ACTIVE");
+        activeButton.setLayoutX(24);
+        activeButton.setLayoutY(79);
+        activeButton.setPrefSize(90,39);
+        activeButton.setStyle("-fx-background-color: lightblue;"+"-fx-font-size: 17");
+        root.getChildren().add(activeButton);
+
+        Button allButton=new Button("ALL");
+        allButton.setLayoutX(266);
+        allButton.setLayoutY(79);
+        allButton.setPrefSize(90,39);
+        allButton.setStyle("-fx-background-color: lightblue;"+"-fx-font-size: 17");
+        root.getChildren().add(allButton);
 
         TextField textField= new TextField();
         textField.setLayoutX(14);
-        textField.setLayoutY(401);
+        textField.setLayoutY(434);
         textField.setPrefSize(252,52);
         textField.setText("Type Message");
         root.getChildren().add(textField);
 
         Button sendButton= new Button("SEND");
-        sendButton.setLayoutY(401);
-        sendButton.setLayoutX(284);
+        sendButton.setLayoutY(434);
+        sendButton.setLayoutX(285);
         sendButton.setPrefSize(69,52);
         sendButton.setStyle("-fx-font-size: 18;"+"-fx-background-color: yellow");
         root.getChildren().add(sendButton);
 
-        VBox messageHolder= new VBox();
-        messageHolder.setLayoutX(10);
-        messageHolder.setLayoutY(206);
-        messageHolder.setPrefSize(360,188);
-        messageHolder.setStyle("-fx-background-color: white;"+"-fx-background-radius: 10");
+        ScrollPane messageHolder= new ScrollPane();
+        messageHolder.setLayoutX(11);
+        messageHolder.setLayoutY(226);
+        messageHolder.setPrefSize(360,200);
+        messageHolder.setStyle("-fx-background-color: white");
+        messageHolder.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         root.getChildren().add(messageHolder);
 
         ScrollPane userList=new ScrollPane();
-        userList.setLayoutX(10);
-        userList.setLayoutY(112);
+        userList.setLayoutX(11);
+        userList.setLayoutY(128);
         userList.setPrefSize(360,88);
         userList.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         root.getChildren().add(userList);
 
         Pane userHolder=new Pane();
-        userHolder.setLayoutX(90);
-        userHolder.setLayoutY(14);
-        userHolder.setPrefSize(210,42);
+        userHolder.setLayoutX(24);
+        userHolder.setLayoutY(25);
+        userHolder.setPrefSize(336,52);
         userHolder.setStyle("-fx-background-color: lightblue;"+"-fx-border-radius: 10;"+"-fx-background-radius: 10");
         Text userName=new Text("name");
-        userName.setLayoutX(90);
-        userName.setLayoutY(27);
+        userName.setLayoutX(145);
+        userName.setLayoutY(30);
         userName.setStyle("-fx-font-size: 16");
         userHolder.getChildren().add(userName);
         root.getChildren().add(userHolder);
 
-        TupleSpace users=new HashMapTupleSpace();
-
-
-
-
+        Messages messages=new Messages();
+        sendButton.setOnMousePressed(e->{
+            messages.addMessage(textField.getText());
+            Text message=new Text(messages.getMessages());
+            messageHolder.setContent(message);
+        });
 
         Scene scene= new Scene(root);
         primaryStage.setScene(scene);

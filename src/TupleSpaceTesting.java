53644dab0d8932b219229a6d73166af680f491f4
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2018-10-05 22:31:08 -0600

    updating tupleTesting

--- src/TupleSpaceTesting.java
+++ src/TupleSpaceTesting.java
@@ -1,35 +1,55 @@
 package sample;
 
+/**
+ * Has a main class that method used for testing can be called with users
+ * choice of # of tuples and the implementation of their choosing.
+ */
 public class TupleSpaceTesting {
-    public static void main(String[]args){
+    /**
+     * Tuples of size 1, 5, and 6 are added each numOfTuples times. Space is then searched numOfTuples/10 times
+     * and also removed from numOfTuples/10 times. Prints out runtime of all the operations at the end of the
+     * method.
+     * @param numOfTuples Int
+     * @param tupleSpace TupleSpace
+     */
+    public static void spaceTesting(int numOfTuples,TupleSpace tupleSpace){
         int i=0;
-        TupleSpace map=new HashMapTupleSpace();
+        TupleSpace map=tupleSpace;
         long runTime=System.currentTimeMillis();
-        while(i<1000){
+        while(i<numOfTuples){
             Tuple tuple=new Tuple(i);
             map.add(tuple);
             i++;
         }
         i=0;
-        while(i<1000){
-            Tuple tuple=new Tuple(i,"this");
+        while(i<numOfTuples){
+            Tuple tuple=new Tuple(i,"this",false,44.4,"string"+i,'c');
             map.add(tuple);
             i++;
         }
         i=0;
-        while(i<500){
+        while(i<numOfTuples){
+            Tuple tuple=new Tuple("string",false,true,10.0,i);
+            map.add(tuple);
+            i++;
+        }
+        i=0;
+        while(i<numOfTuples/10){
             Tuple tuple=new Tuple(i);
             map.remove(i);
             i++;
         }
-        Tuple tuple=new Tuple(3);
-        map.read(3).printTuple();
-        map.remove(2,"apple");
-        map.remove(3,"*");
+        i=0;
+        while(i<numOfTuples/10){
+            Tuple tuple=map.read("*","this",false,44.4,"*",'c');
+            i++;
+        }
         runTime=System.currentTimeMillis()-runTime;
-        System.out.println(runTime+" millis");
-        System.out.println(((HashMapTupleSpace) map).map.get(1).size());
-        System.out.println(((HashMapTupleSpace) map).map.get(2).size());
+        System.out.println(runTime+" milliseconds");
+    }
 
+    public static void main(String[]args){
+//        spaceTesting(30000,new LinkedListTupleSpace());
+//        spaceTesting(30000,new HashMapTupleSpace());
     }
 }
